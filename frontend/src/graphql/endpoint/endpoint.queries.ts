4bb29da39f432851592a370fd28e0bb0076c9be7
Andrea Pizzato <dev@andreapizzato.com>
2021-08-06 19:52:31 +0200

    feat(endpoint): Add Endpoint GraphQL query in frontend

---
+++ frontend/src/graphql/endpoint/endpoint.queries.ts
@@ -0,0 +1,14 @@
+import { gql } from 'graphql-tag';
+
+export type {
+  Endpoint,
+  NetworkType,
+} from '../../../../backend/src/domain/endpoint';
+
+export const GET_ENDPOINT = gql`
+query GetEndpoint {
+  endpoint {
+    url
+    protocolVersion
+  }
+}`;
