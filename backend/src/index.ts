d4350b05621f74a5f3fe9e05cd24dc00e2ffd7e0
Andrea Pizzato <dev@andreapizzato.com>
2021-08-10 18:48:27 +0200

    fix(backend): Fix type exports and served folder path generation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import serveStatic from 'serve-static';
 import open from 'open';
+import path from 'path';
 import { createApolloServer, schema } from './graphql';
 import { TezosBeaconRemoteContext, TezosBeaconRemoteOptions } from './types';
 import { NetworkType } from './domain/endpoint';
 import { Contract } from "./domain/contract";
 import { Endpoint } from "./domain/endpoint";
 
-export type {
+export {
   TezosBeaconRemoteOptions,
   TezosBeaconRemoteContext,
   NetworkType,
@@ -40,7 +41,7 @@ export async function launchTezosBeaconRemote(context: TezosBeaconRemoteContext,
   }));
 
   // Serve frontend bundle
-  app.use(serveStatic('./frontend'));
+  app.use(serveStatic(path.resolve(__dirname, 'frontend')));
 
   // Server startup
   await new Promise<void>(resolve => app.listen({ port: port }, () => resolve()));
